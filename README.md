60af292a5730475a61251117b2c2f4524a939a5a
Simon Halter <simon.halter@fr.clara.net>
2019-04-18 16:19:23 +0200

    initial release

---
+++ README.md
@@ -0,0 +1,5 @@
+# Trollock
+
+This script lock your computer but show your current screen as background image. You can troll your collaborator into believing that you forgot to lock your screen
+
+Dependencies : scrot and i3lock
---
+++ trollock.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#  Copyright 2019 Fivox
+#
+#  Licensed under the WTFPL, Version 2
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+# TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+# 0. You just DO WHAT THE FUCK YOU WANT TO.
+
+# This script lock your computer but show your current screen as background
+# image. You can troll your collaborator into believing that you forgot to
+# lock your screen
+# Dependencies : scrot and i3lock
+
+# Get a path and a temporary file name in /tmp
+temp=$(tempfile).png
+
+# Take a screenshot in PNG format
+scrot "$temp"
+
+# Lock the screen and display the PNG file as a background image
+i3lock -i "$temp"
+
+# Deletes the temporary image file
+rm "$temp"
+
+exit 0
